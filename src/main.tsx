f3c40f6f7532a8fd2a9dcae54eac16b1e519196f
lwshakib <leadwithshakib@gmail.com>
2025-08-15 08:02:14 +0600

    RELEASE 6.0

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import DocumentProvider from "./context/document-provider";
+import "./index.css";
+import { Toaster } from "./components/ui/sonner.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <DocumentProvider>
+      <App />
+      <Toaster />
+    </DocumentProvider>
+  </React.StrictMode>
+);
 
 // Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
-})
+window.ipcRenderer.on("main-process-message", (_event, message) => {
+  console.log(message);
+});
