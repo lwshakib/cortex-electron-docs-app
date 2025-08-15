c1710019297f1cf5190c3357a0cc96c7cebe0eaa
lwshakib <leadwithshakib@gmail.com>
2025-08-15 08:01:47 +0600

    RELEASE 5.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
+import DocumentContent from "./components/document-content";
+import DocumentSidebar from "./components/document-sidebar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "./components/ui/resizable";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen bg-background/90 backdrop-blur-md relative">
+      <ResizablePanelGroup direction="horizontal" className="h-screen">
+        <ResizablePanel
+          defaultSize={25}
+          minSize={20}
+          maxSize={40}
+          className="h-full"
+        >
+          <DocumentSidebar />
+        </ResizablePanel>
+
+        <ResizableHandle withHandle />
+
+        <ResizablePanel defaultSize={75} minSize={30} className="h-full">
+          <DocumentContent />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
 }
 
-export default App
+export default App;
